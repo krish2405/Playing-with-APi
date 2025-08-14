409d53dabb798c17e5c95aba3fe72ef41e3a6b0c
Kishlay Kumar <krishraj2408@gmail.com>
2025-08-15 02:08:41 +0530

    Created APIview class removed function based view

--- breed/urls.py
+++ breed/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import all_dogs,user_by_id
+from .views import AllDogsAV, DogBYidAV
 
 
 urlpatterns = [
-    path("all_dogs",all_dogs,name='all_dogs'),
-    path("<int:pk>",user_by_id,name='user_by_id')
+    path("all_dogs",AllDogsAV.as_view(),name='all_dogs'),
+    path("<int:pk>",DogBYidAV.as_view(),name='user_by_id')
 ]
--- breed/views.py
+++ breed/views.py
@@ -2,45 +2,94 @@ from django.shortcuts import render
 from .models import Breed
 from .serializer import BreedSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
-# Create your views here.
 
-@api_view(['GET','POST'])
-def all_dogs(request):
-    if request.method=='POST':
+class AllDogsAV(APIView):
+    def get(self,request):
+        dogs=Breed.objects.all()
+        serializer=BreedSerializer(dogs,many=True)
+        return Response(serializer.data,status='200')
+    
+
+    def post(self,request):
         serializer=BreedSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=201)
         return Response(serializer.errors,status=400)
-        
-    dogs=Breed.objects.all()
-    serializer=BreedSerializer(dogs,many=True)
-    return Response(serializer.data,status='200')
-
-
-@api_view(['GET','PUT','DELETE'])
-def user_by_id(request,pk):
-    try:
-        dog=Breed.objects.get(pk=pk)
-    except Breed.DoesNotExist:
-        return Response(status=404)
     
-     
-    serializer=BreedSerializer(dog)
-    if request.method=='DELETE':
+class DogBYidAV(APIView):
+    def get(self,request,pk):
+        try:
+            dog=Breed.objects.get(pk=pk)
+        except Breed.DoesNotExist:
+            return Response(status=404)
         
-        dog.delete()
-        return Response(serializer.data, status=204)
+        serializer=BreedSerializer(dog)
+        return Response(serializer.data,status=200)
     
-    if request.method=='PUT':
+
+    def put(self,request,pk):
+        try:
+            dog=Breed.objects.get(pk=pk)
+        except Breed.DoesNotExist:
+            return Response(status=404)
         serializer=BreedSerializer(instance=dog,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status='200')
-        return Response(serializer.errors,status='300')
+        return Response(serializer.errors,status=400)
+    
+    def delete(self,request,pk):
+        try:
+            dog=Breed.objects.get(pk=pk)
+        except Breed.DoesNotExist:
+            return Response(status=404)
+        serializer=BreedSerializer(dog)
+        dog.delete()
+        return Response(serializer.data, status=204)
+
+
+##Using APIViewclass based views
+# Create your views here.
+
+# @api_view(['GET','POST'])
+# def all_dogs(request):
+#     if request.method=='POST':
+#         serializer=BreedSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=201)
+#         return Response(serializer.errors,status=400)
+        
+#     dogs=Breed.objects.all()
+#     serializer=BreedSerializer(dogs,many=True)
+#     return Response(serializer.data,status='200')
+
+
+# @api_view(['GET','PUT','DELETE'])
+# def user_by_id(request,pk):
+#     try:
+#         dog=Breed.objects.get(pk=pk)
+#     except Breed.DoesNotExist:
+#         return Response(status=404)
+    
+     
+#     serializer=BreedSerializer(dog)
+#     if request.method=='DELETE':
+        
+#         dog.delete()
+#         return Response(serializer.data, status=204)
+    
+#     if request.method=='PUT':
+#         serializer=BreedSerializer(instance=dog,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status='200')
+#         return Response(serializer.errors,status='300')
 
     
     
-    return Response(serializer.data,status=200)
+#     return Response(serializer.data,status=200)
