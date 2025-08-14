dbdb88c3a908f727026d2cd271305cb2e3c5de02
Kishlay Kumar <krishraj2408@gmail.com>
2025-08-14 22:51:03 +0530

    string function created in model and name field is updated

--- breed/models.py
+++ breed/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Breed(models.Model):
-    name=models.CharField(max_length=100)
+    name=models.CharField(max_length=100,required=False)
     breed=models.CharField(max_length=100,required=False)
     age=models.IntegerField(required=False)
     weight=models.FloatField(required=False)
+
+    def __str__(self): 
+        return f'{self.name} is breed of {self.breed}' 
