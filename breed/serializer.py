ca6004aa7b90e3038c22b6218700a04ee3dabc06
krish2405 <krishraj2408@gmail.com>
2025-08-15 07:58:53 +0530

    addedmodel serializer

--- breed/serializer.py
+++ breed/serializer.py
@@ -7,26 +7,14 @@ def age_weight(value):
         raise serializers.ValidationError("Invalid value,less than equal to 0 not expected")
     return value
 
-class BreedSerializer(serializers.Serializer):
-    id=serializers.IntegerField(read_only=True)
-    name=serializers.CharField(max_length=100,required=True)
-    breed=serializers.CharField(max_length=100,required=False)
-    age=serializers.IntegerField(required=False,validators=[age_weight])
-    weight=serializers.FloatField(required=False,validators=[age_weight])
+class BreedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Breed
+        # fields="__all__"
+        fields=['id','name']
+        read_only_fields=['id']
 
-    def create(self,validated_data):
-        return Breed.objects.create(**validated_data) 
-    
-    def update(self,instance,data):
-        instance.name=data.get('name',instance.name)
-        instance.breed=data.get('breed',instance.breed)
-        instance.age=data.get('age',instance.age)
-        instance.weight=data.get('weight',instance.weight)
-        instance.save()
-
-        return instance
-    
-    #feild level validation
+      #feild level validation
     def validate_name(self,value):
         if len(value)<3:
             raise serializers.ValidationError("Name must be at least 3 characters long")
@@ -37,3 +25,36 @@ class BreedSerializer(serializers.Serializer):
         if data.get("name")==data.get('breed'):
             raise serializers.ValidationError("Name and breed cannot be same")
         return data
+
+
+# class BreedSerializer(serializers.Serializer):
+
+    # id=serializers.IntegerField(read_only=True)
+    # name=serializers.CharField(max_length=100,required=True)
+    # breed=serializers.CharField(max_length=100,required=False)
+    # age=serializers.IntegerField(required=False,validators=[age_weight])
+    # weight=serializers.FloatField(required=False,validators=[age_weight])
+
+    # def create(self,validated_data):
+    #     return Breed.objects.create(**validated_data) 
+    
+    # def update(self,instance,data):
+    #     instance.name=data.get('name',instance.name)
+    #     instance.breed=data.get('breed',instance.breed)
+    #     instance.age=data.get('age',instance.age)
+    #     instance.weight=data.get('weight',instance.weight)
+    #     instance.save()
+
+    #     return instance
+    
+    # #feild level validation
+    # def validate_name(self,value):
+    #     if len(value)<3:
+    #         raise serializers.ValidationError("Name must be at least 3 characters long")
+    #     return value
+    
+    # #object level validations
+    # def validate(self,data):
+    #     if data.get("name")==data.get('breed'):
+    #         raise serializers.ValidationError("Name and breed cannot be same")
+    #     return data
