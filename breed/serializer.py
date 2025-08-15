a6fb8cda88eb3f26231129565656bab3b5fbc2fa
Kishlay Kumar <krishraj2408@gmail.com>
2025-08-15 11:42:16 +0530

    added differnet type of validator (field level,object level and validator)

--- breed/serializer.py
+++ breed/serializer.py
@@ -1,11 +1,17 @@
-from rest_framework import serializers
+from rest_framework import serializers,validators
 from .models import Breed
+
+
+def age_weight(value):
+    if value<=0:
+        raise serializers.ValidationError("Invalid value,less than equal to 0 not expected")
+    return value
 
 class BreedSerializer(serializers.Serializer):
     name=serializers.CharField(max_length=100,required=True)
     breed=serializers.CharField(max_length=100,required=False)
-    age=serializers.IntegerField(required=False)
-    weight=serializers.FloatField(required=False)
+    age=serializers.IntegerField(required=False,validators=[age_weight])
+    weight=serializers.FloatField(required=False,validators=[age_weight])
 
     def create(self,validated_data):
         return Breed.objects.create(**validated_data) 
@@ -18,3 +24,15 @@ class BreedSerializer(serializers.Serializer):
         instance.save()
 
         return instance
+    
+    #feild level validation
+    def validate_name(self,value):
+        if len(value)<3:
+            raise serializers.ValidationError("Name must be at least 3 characters long")
+        return value
+    
+    #object level validations
+    def validate(self,data):
+        if data.get("name")==data.get('breed'):
+            raise serializers.ValidationError("Name and breed cannot be same")
+        return data
